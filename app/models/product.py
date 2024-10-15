107fc0907f36a0403b20ff370cd1cda36be4d57d
Giyoung Kim <gk122@duke.edu>
2024-10-15 18:06:14 -0400

    Updated Products relation

--- app/models/product.py
+++ app/models/product.py
@@ -2,28 +2,30 @@ from flask import current_app as app
 
 
 class Product:
-    def __init__(self, id, name, price, available):
+    def __init__(self, id, name, description, available):
         self.id = id
         self.name = name
-        self.price = price
+        self.description = description
         self.available = available
 
     @staticmethod
     def get(id):
-        rows = app.db.execute('''
-SELECT id, name, price, available
-FROM Products
-WHERE id = :id
-''',
-                              id=id)
+        rows = app.db.execute(
+            '''
+            SELECT id, name, description, available
+            FROM Products
+            WHERE id = :id
+            ''',
+            id=id)
         return Product(*(rows[0])) if rows is not None else None
 
     @staticmethod
     def get_all(available=True):
-        rows = app.db.execute('''
-SELECT id, name, price, available
-FROM Products
-WHERE available = :available
-''',
-                              available=available)
+        rows = app.db.execute(
+            '''
+            SELECT id, name, description, available
+            FROM Products
+            WHERE available = :available
+            ''',
+            available=available)
         return [Product(*row) for row in rows]
