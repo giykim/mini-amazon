995c7048d1fae0997814ff830e2c56680cb61f46
Jason Ren <jason.ren@duke.edu>
2024-10-16 01:40:06 -0400

    Added reviews, productreviews, sellerreviews. Added function get_recent_reviews to get most recent reviews for user.

---
+++ app/models/reviews.py
@@ -0,0 +1,46 @@
+from flask import current_app as app
+
+
+class Review:
+    def __init__(self, id, rating, description, time_created, helpfulness):
+        self.id = id
+        self.rating = rating
+        self.description = description
+        self.time_created = time_created
+        self.helpfulness = helpfulness
+
+    @staticmethod
+    def get(id):
+        rows = app.db.execute('''
+            SELECT id, rating, description, time_created, helpfulness
+            FROM Reviews
+            WHERE id = :id
+            ''',
+            id=id)
+        return Review(*(rows[0])) if rows is not None else None
+    
+    @staticmethod
+    def get_recent_reviews(reviewerid):
+        rows = app.db.execute("""
+            SELECT 
+                CASE
+                 WHEN p.pid IS NULL THEN 'Seller'
+                 ELSE 'Product'
+                END AS review_type,
+                CASE
+                 WHEN p1.name IS NULL THEN u.firstname || ' ' || u.lastname
+                 ELSE p1.name
+                END AS Name,
+                r.rating, r.description, r.time_created, r.helpfulness
+            FROM Reviews r
+            LEFT JOIN SellerReviews s ON r.id = s.id
+            LEFT JOIN ProductReviews p ON r.id = p.id
+            LEFT JOIN Products AS p1 ON p1.id = p.pid
+            LEFT JOIN Users AS u ON u.id = s.sid
+            WHERE s.uid = :reviewerid or p.uid = :reviewerid
+            ORDER BY r.time_created DESC
+            LIMIT 5
+            """,
+            reviewerid = reviewerid
+        )
+        return rows
